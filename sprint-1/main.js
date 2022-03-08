8b45d9c55452a20a33f944df4121566a901f9c3b
spiderq2697 <spiderq10@gmail.com>
2022-03-09 00:04:22 +0900

    fix: main에 import, testCase 추가

--- sprint-1/main.js
+++ sprint-1/main.js
@@ -1,5 +1,13 @@
-class Log {
-    constructor() {}
-  }
-  
-  module.exports = TimeConverter;  
+const TimeConverter = require("./domain/time-converter");
+const Log = require("./util/log");
+const testCase = [
+  "1600s m",
+  "1h35m s",
+  "3d h",
+  "3837s h",
+  "1400s",
+  "1700H m",
+  "1 1",
+  "123 s",
+  "12h 2",
+];

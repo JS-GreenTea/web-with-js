3eabb0406edac39f5fcab678054f1d46c0d429f7
2yunseong <dbsdltjd3701@naver.com>
2022-03-14 05:00:25 +0900

    feat : command 파싱 및 콜백함수, 테스트 배열 추출 로직 작성

--- sprint-2/domain/commander.js
+++ sprint-2/domain/commander.js
@@ -1,9 +1,33 @@
 class Commander {
-  constructor() {}
+  constructor(command) {
+    this.command = command;
+  }
 
-  parseCommand = (command) => {
-    const commandArray = command.split();
+  parseCommand() {
+    const parsedCommandArray = this.command.split(" ");
     const option = parsedCommandArray[0];
-  };
+    const operateElements = parsedCommandArray.slice(1);
+
+    this.option = option;
+    this.operateElements = operateElements;
+  }
+
+  giveCallBackFuncAndOperateElementsByOption() {
+    if (this.option === "add") {
+      return [this.add, this.convertStringElementToNumber()];
+    }
+    if (this.option === "count") {
+      return [this.count, this.operateElements];
+    }
+  }
+
+  convertStringElementToNumber() {
+    return this.operateElements.map((e) => Number(e));
+  }
+
+  count() {}
+  add(acc, cur) {
+    return acc + cur;
+  }
 }
 module.exports = Commander;

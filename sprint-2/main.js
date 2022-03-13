1ea15f83656aa5e0d2782d2eea20d6eae7a50df1
2yunseong <dbsdltjd3701@naver.com>
2022-03-14 05:05:44 +0900

    feat : testCase 추가

--- sprint-2/main.js
+++ sprint-2/main.js
@@ -3,7 +3,12 @@ const Log = require("./util/log");
 const Array = require("./domain/array");
 
 function main() {
-  const inputData = ["count 1 2 3 4 5", "add 35 53"];
+  const inputData = [
+    "count 1 2 3 4 5",
+    "add 35 53",
+    "add 1 2 3 4 5 6 7 8 9 10",
+    "count 1 가 나 다 라 xx df 8개",
+  ];
   const testData = [1, 2, 3, 4, 5];
 
   for (let i = 0; i < inputData.length; i++) {

981f688013bd592d6df14a95b9eb2a6f92eff7a4
2yunseong <dbsdltjd3701@naver.com>
2022-03-14 05:04:20 +0900

    feat : main 프로그램 로직 작성

--- sprint-2/main.js
+++ sprint-2/main.js
@@ -1,6 +1,18 @@
 const Commander = require("./domain/commander");
 const Log = require("./util/log");
+const Array = require("./domain/array");
+
 function main() {
   const inputData = ["count 1 2 3 4 5", "add 35 53"];
+  const testData = [1, 2, 3, 4, 5];
+
+  for (let i = 0; i < inputData.length; i++) {
+    const commander = new Commander(inputData[i]);
+    commander.parseCommand();
+    const [callbackFunction, testArray] =
+      commander.giveCallBackFuncAndOperateElementsByOption();
+    const resultLog = new Log(testArray.customReduce(callbackFunction, 0));
+    resultLog.print();
+  }
 }
 main();

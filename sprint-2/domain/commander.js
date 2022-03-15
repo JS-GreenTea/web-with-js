9a07aa2b2d308e594225f84f577410093d1f5a11
spiderq2697 <spiderq10@gmail.com>
2022-03-15 21:08:22 +0900

    feat: 명령어 따라 각각에 맞는 기능 실행 메서드 추가 - 명령어가 'count'일 때 count() 실행 - 명령어가 'add'일 때 add() 실행

--- sprint-2/domain/commander.js
+++ sprint-2/domain/commander.js
@@ -9,11 +9,21 @@ class Commander {
     this.inputValue = inputValue;
   }
 
-  separateNumbersCommands(inputValue) {
-    const arr = inputValue.split(' ');
+  separateNumbersCommands() {
+    const arr = this.inputValue.split(' ');
     this.command = arr[0];
     for(let i = 1; i < arr.length; i++) {
       this.numbers.push(arr[i]);
+    }
+  }
+
+  selectCommands() {
+    if (this.command === 'count') {
+      this.count();
+    }
+
+    if (this.command === 'add') {
+      this.add();
     }
   }
 }

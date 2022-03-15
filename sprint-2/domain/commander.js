94c20c21471b1657102d3d7d3d8905538ce5cd88
spiderq2697 <spiderq10@gmail.com>
2022-03-15 21:02:06 +0900

    feat: 명령어와 number list를 분리하는 메서드 추가

--- sprint-2/domain/commander.js
+++ sprint-2/domain/commander.js
@@ -1,5 +1,20 @@
 const array = require("./array.js");
+
 class Commander {
-  constructor(params) {}
+  inputValue = '';
+  command = '';
+  numbers = [];
+
+  constructor(inputValue) {
+    this.inputValue = inputValue;
+  }
+
+  separateNumbersCommands(inputValue) {
+    const arr = inputValue.split(' ');
+    this.command = arr[0];
+    for(let i = 1; i < arr.length; i++) {
+      this.numbers.push(arr[i]);
+    }
+  }
 }
 module.exports = Commander;

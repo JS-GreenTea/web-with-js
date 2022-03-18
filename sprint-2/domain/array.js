9ae75e80a18b4950a72624e847ea395fc25d8541
spiderq2697 <spiderq10@gmail.com>
2022-03-18 19:33:30 +0900

    feat: custom Reduce 구현

--- sprint-2/domain/array.js
+++ sprint-2/domain/array.js
@@ -1,5 +1,21 @@
-Array.prototype.customReduce = function() {
-  return 0;
+Array.prototype.customReduce = function(callback, initialValue = 0) {
+  let array = Object(this);
+  const length = array.length;
+  let accumulator = array[0];
+  let index = 1;
+
+
+  if (initialValue !== 0) {
+    accumulator = initialValue;
+    index = 0;
+  }
+
+  while(index < length) {
+    let currentValue = array[index];
+    accumulator = callback(accumulator, currentValue, currentIndex = index, array = array);
+    index++;
+  }
+  return accumulator;
 };
 
 module.exports = Array;

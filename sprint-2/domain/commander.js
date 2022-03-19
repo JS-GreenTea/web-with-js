e4b086a7df70ae3741fb99759ef515b6c0154c51
gxxrxn <kkr1226@naver.com>
2022-03-19 19:40:37 +0900

    feat: Commander 명령어 파싱 기능
    - Commander 클래스의 parse 함수 구현
    - 명령어를 공백에 따라 split
    - add 명령인 경우 값들을 숫자로 변환
    - Commander의 명령 옵션으로 add, count를 가지도록 구현

--- sprint-2/domain/commander.js
+++ sprint-2/domain/commander.js
@@ -1,5 +1,20 @@
 const array = require("./array.js");
+const { add, count } = require("./operations.js");
+
 class Commander {
-  constructor(params) {}
+  constructor() {
+    this.options = { add, count };
+  }
+
+  parse(command) {
+    let [option, ...array] = command.split(" ");
+
+    if (option == "add") {
+      array = array.map((x) => Number(x));
+    }
+
+    return [option, array];
+  }
 }
+
 module.exports = Commander;

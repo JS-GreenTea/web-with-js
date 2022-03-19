0688ab5b5c105e087b8d9cf67812cf0d890e674b
gxxrxn <kkr1226@naver.com>
2022-03-19 19:56:25 +0900

    feat: testcase에 따라 commander 실행하는 main 함수 구현

--- sprint-2/main.js
+++ sprint-2/main.js
@@ -1,10 +1,15 @@
 const Commander = require("./domain/commander");
 const Log = require("./util/log");
-const { add, count } = require("./domain/operations");
 
 function main() {
-  console.log(add([1, 2, 3]));
-  console.log(count([1, 2, 3]));
+  const commander = new Commander();
+  const log = new Log();
+  const testCase = ["count 1 2 3 4 5", "add 35 23"];
+
+  testCase.forEach((tc) => {
+    const [option, result] = commander.run(tc);
+    log.result(option, result);
+  });
 }
 
 main();

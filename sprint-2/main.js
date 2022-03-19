8a14891de3a4321f951b3a5fc666edcb16efa32f
gxxrxn <kkr1226@naver.com>
2022-03-19 18:09:56 +0900

    feat: add, count 함수 구현
    - operations.js에 customReduce를 사용하는 add, count 함수 구현

--- sprint-2/main.js
+++ sprint-2/main.js
@@ -1,8 +1,10 @@
 const Commander = require("./domain/commander");
 const Log = require("./util/log");
+const { add, count } = require("./domain/operations");
 
 function main() {
-  console.log([1, 2, 3].customReduce((acc, cur) => acc + cur));
+  console.log(add([1, 2, 3]));
+  console.log(count([1, 2, 3]));
 }
 
 main();

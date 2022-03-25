ccf5e2178d44f9f484889b3feaa61d5916dea295
gxxrxn <kkr1226@naver.com>
2022-03-25 15:00:52 +0900

    feat: Shop, Employee 클래스 구현
    - shop의 order 메소드 구현
    - employee의 makeBurger 메소드 구현

--- sprint-3/domain/employee.js
+++ sprint-3/domain/employee.js
@@ -1,1 +1,11 @@
-class Employee {}
+class Employee {
+  constructor(name) {
+    this.name = name;
+  }
+
+  makeBurger(time) {
+    console.log(`${time / 1000}: 햄버거를 완성하였습니다.`);
+  }
+}
+
+module.exports = Employee;
--- sprint-3/domain/shop.js
+++ sprint-3/domain/shop.js
@@ -1,4 +1,25 @@
+const Employee = require("./employee.js");
+
 class Shop {
-  constructor() {}
-  order() {}
+  constructor() {
+    this.employees = [new Employee("fry"), new Employee("yun")];
+    this.makingTime = 0;
+  }
+
+  order(count) {
+    const intervalID = setInterval(() => {
+      this.makingTime += 2000;
+      this.employees.forEach((employee) => {
+        if (count <= 0) {
+          console.log("주문이 완료되었습니다.");
+          clearInterval(intervalID);
+          return;
+        }
+        employee.makeBurger(this.makingTime);
+        count--;
+      });
+    }, 2000);
+  }
 }
+
+module.exports = Shop;

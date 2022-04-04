380de313e4e6b88ff9639d87966280dd2c1b466c
KINHYEONJI <hyeonji0718@gmail.com>
2022-04-04 15:47:37 +0900

    feat: 동기함수와 비동기함수를 만든다. - syncf 동기 함수 - asyncf 비동기 함수

--- sprint-3/sync-async.js
+++ sprint-3/sync-async.js
@@ -1,6 +1,31 @@
-function syncf() {}
+let start = new Date().getTime();
+function syncf(numOrder, delayTime) {
+  let tmpStart = new Date().getTime();
+  let end = 0;
+  let result = 0;
 
-function asyncf() {}
+  while (end - tmpStart < delayTime) {
+    end = new Date().getTime();
+  }
+
+  result = end - start;
+  result = parseInt(result / 1000);
+
+  console.log(result, numOrder);
+}
+
+function asyncf(numOrder, delayTime) {
+  let end = 0;
+  let tmpTime = 0;
+
+  setTimeout(() => {
+    end = new Date().getTime();
+    tmpTime = end - start;
+    tmpTime = parseInt(tmpTime / 1000);
+
+    console.log(tmpTime, numOrder);
+  }, delayTime);
+}
 
 syncf("first", 1000);
 asyncf("second", 1000);

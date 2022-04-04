dbd4975c29a8cf84d2ad5dd0500097eac3602a39
KINHYEONJI <hyeonji0718@gmail.com>
2022-04-04 17:53:37 +0900

    feat: 전체 동작을 실행한다
    - shop 매개변수에 직원 두명과 시작시간을 넘긴다

--- sprint-3/main.js
+++ sprint-3/main.js
@@ -1,1 +1,12 @@
-function main() {}
+const Employee = require("./domain/employee.js");
+const Shop = require("./domain/shop.js");
+
+function main() {
+  const start = new Date().getTime();
+  const employee1 = new Employee();
+  const employee2 = new Employee();
+  const shop = new Shop(employee1, employee2, start);
+  shop.order(5);
+}
+
+main();

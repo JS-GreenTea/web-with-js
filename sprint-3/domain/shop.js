f826341a15089d69d1adb04fa074be832d93fd7e
KINHYEONJI <hyeonji0718@gmail.com>
2022-04-04 17:52:11 +0900

    feat: 버거를 주문받는 order함수를 추가한다
    - 직원마다 햄버거를 반복해서 만드는 makeBurgerInterval을 생성한다

--- sprint-3/domain/shop.js
+++ sprint-3/domain/shop.js
@@ -5,7 +5,31 @@ class Shop {
     this.start = start;
   }
 
-  order() {}
+  order(burgerNum) {
+    const makeBurgerInterval1 = setInterval(() => {
+      if (burgerNum > 0) {
+        let functionEnd = new Date().getTime();
+        let result = functionEnd - this.start;
+        result = parseInt(result / 1000);
+        burgerNum -= 1;
+        this.employee1.makeBurger(result);
+      } else if (burgerNum <= 0) {
+        clearInterval(makeBurgerInterval1);
+      }
+    }, 2000);
+
+    const makeBurgerInterval2 = setInterval(() => {
+      if (burgerNum > 0) {
+        let functionEnd = new Date().getTime();
+        let result = functionEnd - this.start;
+        result = parseInt(result / 1000);
+        burgerNum -= 1;
+        this.employee2.makeBurger(result);
+      } else if (burgerNum <= 0) {
+        clearInterval(makeBurgerInterval2);
+      }
+    }, 2000);
+  }
 }
 
 module.exports = Shop;

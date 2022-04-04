7cd1e52c002746b871b0b93a611a300562f63a90
KINHYEONJI <hyeonji0718@gmail.com>
2022-04-04 17:45:53 +0900

    feat: 생성자함수 작성
    -직원 2명과 시작시간을 매개변수로 전달

--- sprint-3/domain/shop.js
+++ sprint-3/domain/shop.js
@@ -1,4 +1,11 @@
 class Shop {
-  constructor() {}
+  constructor(employee1, employee2, start) {
+    this.employee1 = employee1;
+    this.employee2 = employee2;
+    this.start = start;
+  }
+
   order() {}
 }
+
+module.exports = Shop;

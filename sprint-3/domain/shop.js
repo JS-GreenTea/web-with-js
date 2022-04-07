bdf32d60df4a1f907932710387a2a1e4a2418381
spiderq2697 <spiderq10@gmail.com>
2022-04-07 21:45:11 +0900

    refactor: 함수를 분리하여 가시화한다
    - 햄버거를 만드는 행동(makeBurger), 완성을 알려주는 행동(alarmRunningTimeFor)로 분리한다

--- sprint-3/domain/shop.js
+++ sprint-3/domain/shop.js
@@ -13,26 +13,17 @@ class Shop {
   }
 
   order(burgerNum) {
-    const makeBurgerInterval1 = setInterval(() => {
-      if (burgerNum > 0) {
-        burgerNum -= 1;
-        this.employee1.makeBurger(this.runningTimeFor());
-      } else if (burgerNum <= 0) {
-        clearInterval(makeBurgerInterval1);
-      }
-    }, 2000);
-
-    const makeBurgerInterval2 = setInterval(() => {
-      if (burgerNum > 0) {
-        let functionEnd = new Date().getTime();
-        let result = functionEnd - this.start;
-        result = parseInt(result / 1000);
-        burgerNum -= 1;
-        this.employee2.makeBurger(result);
-      } else if (burgerNum <= 0) {
-        clearInterval(makeBurgerInterval2);
-      }
-    }, 2000);
+    this.employees.forEach((employee) => {
+      const flowInterval = setInterval(() => {
+        if (burgerNum > 0) {
+          employee.makeBurger(burgerNum);
+          burgerNum -= 1;
+          this.alarmRunningTimeFor(employee);
+        } else if (burgerNum === 0) {
+          clearInterval(flowInterval);
+        }
+      }, 2000);
+    });
   }
 }
 

5e65b91f6b5047ebb97eb31e9557414bc47aaa42
spiderq2697 <spiderq10@gmail.com>
2022-04-07 21:46:03 +0900

    refactor: 이름이 있는 직원의 배열을 shop 인스턴스에 추가한다

--- sprint-3/main.js
+++ sprint-3/main.js
@@ -2,10 +2,11 @@ const Employee = require("./domain/employee.js");
 const Shop = require("./domain/shop.js");
 
 function main() {
-  const start = new Date().getTime();
-  const employee1 = new Employee();
-  const employee2 = new Employee();
-  const shop = new Shop(employee1, employee2, start);
+  const employee1 = new Employee('maenji');
+  const employee2 = new Employee('singco');
+  const employees = [employee1, employee2];
+
+  const shop = new Shop(employees);
   shop.order(5);
 }
 

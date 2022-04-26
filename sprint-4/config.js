3d4ef3b43e07d339a2aa1b9c99be87c41c0b27dc
spiderq2697 <spiderq10@gmail.com>
2022-04-26 23:16:51 +0900

    feat: 버거의 정보를 모듈화한다
    - 버거의 이름과 만드는데 걸리는 시간을 객체로 관리한다

---
+++ sprint-4/config.js
@@ -0,0 +1,16 @@
+const burgerInfo = [
+  {
+    name: "햄버거",
+    makingTime: 2,
+  },
+  {
+    name: "치즈버거",
+    makingTime: 3,
+  },
+  {
+    name: "치킨버거",
+    makingTime: 4,
+  },
+];
+
+module.exports = { burgerInfo };

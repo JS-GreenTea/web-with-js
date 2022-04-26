3cf917d13f90cd33dc3bf0464d63cea712131b6f
spiderq2697 <spiderq10@gmail.com>
2022-04-26 23:25:37 +0900

    feat: shop 객체를 생성한다

--- sprint-4/domain/shop.js
+++ sprint-4/domain/shop.js
@@ -1,4 +1,29 @@
+const Employee = require("./employee.js");
+const { burgerInfo } = require("../config.js");
+
 class Shop {
-  constructor() {}
-  order() {}
+  constructor(numberEmployees) {
+    this.orderStartTime = new Date().getTime();
+    this.employees = [];
+
+    const randomPick = Math.floor(Math.random() * burgerInfo.length);
+    console.log(burgerInfo[randomPick]);
+
+    for (let i = 0; i < numberEmployees; i++) {
+      this.employees.push(new Employee(burgerInfo["햄버거"]));
+    }
+  }
+
+  order(menu) {
+    switch (menu) {
+      case "햄버거":
+        break;
+      case "치즈버거":
+        break;
+      case "치킨버거":
+        break;
+    }
+  }
 }
+
+module.exports = Shop;

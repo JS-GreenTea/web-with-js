a9dc00fc7ca61aceedf9e91b838ca4ff6463c3d0
spiderq2697 <spiderq10@gmail.com>
2022-04-28 21:58:56 +0900

    feat: Employee 객체를 생성한다

--- sprint-4/domain/employee.js
+++ sprint-4/domain/employee.js
@@ -1,1 +1,14 @@
-class Employee {}
+class Employee {
+  constructor(burgerSkills) {
+    this.burgerSkills = burgerSkills;
+  }
+
+  makeBurger(burgerName) {
+    if (!burgerName in this.burgerSkills) {
+      return;
+    }
+    return new Burger(burgerName, this.burgerSkills[burgerName]);
+  }
+}
+
+module.exports = Employee;

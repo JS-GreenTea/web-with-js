2e02c66f89263a10a8313a000ab5773e60f8fc9b
spiderq2697 <spiderq10@gmail.com>
2022-04-29 21:21:50 +0900

    feat: 주문한대로 버거를 만든다
    - work 스택에 쌓은 후 work 메서드를 통해 makingTime만큼 기다리는 promise 생성한다

--- sprint-4/domain/employee.js
+++ sprint-4/domain/employee.js
@@ -1,27 +1,36 @@
 class Employee {
-  constructor(burgerSkills, workingTime) {
+  constructor(name, burgerSkills) {
+    this.name = name;
     this.burgerSkills = burgerSkills;
-    this.workingTime = workingTime;
+    this.workingTime = 0;
     this.workStack = [];
   }
 
-  workingTime() {
-    return this.workingTime;
+  makeBurger(burgerName, makingTime, workingTime) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        console.log(`${workingTime + makingTime}초 ${this.name} ${burgerName}`);
+        resolve(workingTime + makingTime);
+      }, 1000 * makingTime);
+    });
   }
 
-  workStack() {
-    return this.workStack;
-  }
+  work() {
+    let promise = this.makeBurger(
+      this.workStack[0],
+      this.burgerSkills[this.workStack[0]],
+      0
+    );
 
-  burgerSkills() {
-    return this.burgerSkills;
-  }
-
-  makeBurger(burgerName) {
-    if (!burgerName in this.burgerSkills) {
-      return;
+    for (let i = 1; i < this.workStack.length; i++) {
+      promise = promise.then((time) =>
+        this.makeBurger(
+          this.workStack[i],
+          this.burgerSkills[this.workStack[i]],
+          time
+        )
+      );
     }
-    return new Burger(burgerName, this.burgerSkills[burgerName]);
   }
 }
 

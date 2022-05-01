f4532e5c507463ad6064ab7b97975ee0d8c4bc8b
2yunseong <dbsdltjd3701@naver.com>
2022-05-01 17:03:00 +0900

    refactor : switch문 문자열 리터럴을 상수로 변경

--- sprint-4/domain/burger.js
+++ sprint-4/domain/burger.js
@@ -1,25 +1,25 @@
-import {HAM, CHICKEN, CHEEZE} from './BURBGER_TYPE';
+import { HAM, CHICKEN, CHEEZE, HAM_STR, CHICKEN_STR, CHEEZE_STR } from './BURBGER_TYPE';
 
 class Burger{
     constructor(burgerName){
         this.burgerType = 0;
         this.makingTime = 0;
-        
+
         switch(burgerName) {
-            case "햄버거":
+            case HAM_STR:
                 this.burgerType = HAM;
                 this.makingTime = 2;    
                 break;
-            case "치킨버거":
+            case CHICKEN_STR:
                 this.burgerType = CHICKEN;
                 this.makingTime = 3;
                 break;
-            case "치즈버거":
-                this.burgerType = CHICKEN;
+            case CHEEZE_STR:
+                this.burgerType = CHEEZE;
                 this.makingTime = 4;
                 break;
             default:
-                console.error("Error!")
+                console.error("Error!");
         }
     }
     getMakingTime() {

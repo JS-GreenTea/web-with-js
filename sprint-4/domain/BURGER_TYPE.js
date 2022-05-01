29a801e16a59cfb1cbe8b417184648cdcb8f52de
2yunseong <dbsdltjd3701@naver.com>
2022-05-01 17:01:48 +0900

    feat : burger string 추가

--- sprint-4/domain/BURGER_TYPE.js
+++ sprint-4/domain/BURGER_TYPE.js
@@ -1,3 +1,6 @@
 module.exports.HAM = 0;
 module.exports.CHICKEN = 1;
 module.exports.CHEEZE = 2;    
+module.exports.HAM_STR = "햄버거";    
+module.exports.CHICKEN_STR = "치킨버거";    
+module.exports.CHEEZE_STR = "치즈버거";    

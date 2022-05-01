db7171f0cf65104d7c621e747d703842dc76e99e
2yunseong <dbsdltjd3701@naver.com>
2022-05-01 15:19:40 +0900

    feat: employee 변수명 설정

--- sprint-4/domain/employee.js
+++ sprint-4/domain/employee.js
@@ -1,1 +1,7 @@
-class Employee {}
+class Employee {
+    constructor(burgerMenu, employeeTag, cycleTime){
+        this.burgerMenu = burgerMenu; // 버거 매뉴
+        this.employeeTag = employeeTag; // 직원 태그 이름
+        this.cycleTime = cycleTime; // 내부에서 돌아가는 자기만의 시간 
+    }
+}

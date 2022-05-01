6c873cc0949a7907adf56de757c3f8436e0f5956
2yunseong <dbsdltjd3701@naver.com>
2022-05-01 20:44:02 +0900

    feat : employee 구현

--- sprint-4/domain/employee.js
+++ sprint-4/domain/employee.js
@@ -1,7 +1,36 @@
-class Employee {
-    constructor(burgerMenu, employeeTag, cycleTime){
-        this.burgerMenu = burgerMenu; // 버거 매뉴
+import {HAM, CHICKEN, CHEEZE} from './BURGER_TYPE.js';
+
+export class Employee {
+    constructor(makingBurgerList, employeeTag){
+        this.makingBurgerList = makingBurgerList; // 직원이 만들 수 있는 버거 타입
         this.employeeTag = employeeTag; // 직원 태그 이름
-        this.cycleTime = cycleTime; // 내부에서 돌아가는 자기만의 시간 
+        this.isWorking = false;
+    }
+    canMakingBurgerTypes(burger){
+        const verificationBurgerList = this.makingBurgerList.filter((b) => {
+            return burger.getBurgerType() === b.getBurgerType();
+        });
+
+        if(verificationBurgerList.length == 0){
+            return false;
+        }
+        return true;
+    }
+
+    getEmployeeTag(){
+        return this.employeeTag;
+    }
+
+    makeBurger(burger, totalTimes) {
+        this.isWorking = true;
+        console.log(burger.getBurgerType()+ "전달된 시간 (동기): " + totalTimes);
+        return new Promise((resolve, reject) =>{
+            setTimeout(() => {
+                console.log(burger.getBurgerType() + "전달된 시간 (비동기): " + totalTimes);
+                console.log(`${totalTimes + burger.makingTime}초 직원${this.employeeTag} ${burger.burgerName}` );
+                this.isWorking = false;
+                resolve();
+            }, burger.makingTime * 1000);
+        });
     }
 }

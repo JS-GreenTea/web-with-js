f7ef280641353a1adbd44c0ae3fb9012a3a16ce1
2yunseong <dbsdltjd3701@naver.com>
2022-05-02 14:06:00 +0900

    refactor : main 세부 로직 추상화

--- sprint-4/main.js
+++ sprint-4/main.js
@@ -10,52 +10,9 @@ function main() {
     let burgers = [];
     let burgerCounts = [];
 
-    for (let i = 0; i < burgerNames.length; i++) {
-        burgers.push(new Burger(burgerNames[i]));
-    }
+    [burgers, burgerCounts] = orderBurgersSetter(burgerNames);
 
-    burgerCounts = burgerCounter(burgers);
-
-    let burgerTypes = [];
-
-    if(employeesNumber === 1){
-        if (burgerCounts[CHEEZE] !== 0) {
-            burgerTypes = [HAM_STR, CHEEZE_STR];
-        } else {
-            burgerTypes = [HAM_STR, CHICKEN_STR];
-        }
-        employees.push(employeeEnrollment(1, burgerTypes));
-    } else if(employeesNumber === 2){
-        if (burgerCounts[CHEEZE] !== 0 && burgerCounts[CHICKEN] === 0) {
-            employees.push(employeeEnrollment(1, [HAM_STR, CHEEZE_STR]));
-            employees.push(employeeEnrollment(2, [HAM_STR, CHEEZE_STR]));
-        } else if(burgerCounts[CHICKEN] !== 0 && burgerCounts[CHEEZE] === 0){
-            employees.push(employeeEnrollment(1, [HAM_STR, CHICKEN_STR]));
-            employees.push(employeeEnrollment(2, [HAM_STR, CHICKEN_STR]));
-        } else {
-            employees.push(employeeEnrollment(1, [HAM_STR, CHEEZE_STR]));
-            employees.push(employeeEnrollment(2, [HAM_STR, CHICKEN_STR]));
-        }
-    } else{
-        let isInputedChicken = false;
-        for(let i = 0; i < employeesNumber; i++) {
-            const EMPLOYEE_TAG = i + 1;
-            if(i == 0){
-                employees.push(employeeEnrollment(EMPLOYEE_TAG, [HAM_STR, CHEEZE_STR]));
-                continue;
-            } else if(i == 1){
-                employees.push(employeeEnrollment(EMPLOYEE_TAG, [HAM_STR, CHICKEN_STR])); 
-                continue;
-            } else if(burgerCounts[CHEEZE] < burgerCounts[CHICKEN] && i == 2){
-                isInputedChicken = true;
-            } 
-
-            if (isInputedChicken) employees.push(employeeEnrollment(EMPLOYEE_TAG, [HAM_STR, CHICKEN_STR])); 
-            else employees.push(employeeEnrollment(EMPLOYEE_TAG, [HAM_STR, CHEEZE_STR])); 
-
-            isInputedChicken = !isInputedChicken;
-        }
-    }    
+    employees = employeesSetter(employeesNumber, burgerCounts);
 
     let shop = new Shop(employees);
     shop.order(burgers);
@@ -94,6 +51,82 @@ function makeBurgerTypes(burgerTypes) {
 function employeeEnrollment(employeeTag, burgerTypes){
     const makingBurgerTypes = makeBurgerTypes(burgerTypes);
     return new Employee(makingBurgerTypes, employeeTag);
+}
+
+function orderBurgersSetter(burgerNames) {
+    let burgers = [];
+    let burgerCounts = [];
+    for (let i = 0; i < burgerNames.length; i++) {
+        burgers.push(new Burger(burgerNames[i]));
+    }
+
+    burgerCounts = burgerCounter(burgers);
+    return [burgers, burgerCounts];
+}
+
+function employeesSetter(employeesNumber, burgerCounts) {
+    let employees = [];
+    
+    if(employeesNumber === 1){
+        employees = oneEmployeeSetter(burgerCounts);
+    } else if(employeesNumber === 2){
+        employees = twoEmployeesSetter(burgerCounts);
+    } else{
+        employees = multipleEmployeesSetter(burgerCounts, employeesNumber);
+    }    
+    
+    return employees;
+}
+
+function oneEmployeeSetter(burgerCounts) {
+    let burgerTypes = [];
+    let employees = [];
+
+    if (burgerCounts[CHEEZE] !== 0) {
+        burgerTypes = [HAM_STR, CHEEZE_STR];
+    } else {
+        burgerTypes = [HAM_STR, CHICKEN_STR];
+    }
+    employees.push(employeeEnrollment(1, burgerTypes));
+    return employees;
+}
+
+function twoEmployeesSetter(burgerCounts) {
+    let employees = [];
+    if (burgerCounts[CHEEZE] !== 0 && burgerCounts[CHICKEN] === 0) {
+        employees.push(employeeEnrollment(1, [HAM_STR, CHEEZE_STR]));
+        employees.push(employeeEnrollment(2, [HAM_STR, CHEEZE_STR]));
+    } else if(burgerCounts[CHICKEN] !== 0 && burgerCounts[CHEEZE] === 0){
+        employees.push(employeeEnrollment(1, [HAM_STR, CHICKEN_STR]));
+        employees.push(employeeEnrollment(2, [HAM_STR, CHICKEN_STR]));
+    } else {
+        employees.push(employeeEnrollment(1, [HAM_STR, CHEEZE_STR]));
+        employees.push(employeeEnrollment(2, [HAM_STR, CHICKEN_STR]));
+    }
+    return employees;
+}
+
+function multipleEmployeesSetter(burgerCounts, employeesNumber){
+    let isInputedChicken = false;
+    let employees = [];
+    for(let i = 0; i < employeesNumber; i++) {
+        const EMPLOYEE_TAG = i + 1;
+        if(i == 0){
+            employees.push(employeeEnrollment(EMPLOYEE_TAG, [HAM_STR, CHEEZE_STR]));
+            continue;
+        } else if(i == 1){
+            employees.push(employeeEnrollment(EMPLOYEE_TAG, [HAM_STR, CHICKEN_STR])); 
+            continue;
+        } else if(burgerCounts[CHEEZE] < burgerCounts[CHICKEN] && i == 2){
+            isInputedChicken = true;
+        } 
+
+        if (isInputedChicken) employees.push(employeeEnrollment(EMPLOYEE_TAG, [HAM_STR, CHICKEN_STR])); 
+        else employees.push(employeeEnrollment(EMPLOYEE_TAG, [HAM_STR, CHEEZE_STR])); 
+
+        isInputedChicken = !isInputedChicken;
+    }
+    return employees;
 }
 
 main();

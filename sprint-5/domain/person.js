cba1200d31080b2cafa8876b05e2371b1ae655f8
spiderq2697 <spiderq10@gmail.com>
2022-05-12 22:34:16 +0900

    feat: currentNum이 3, 6, 9일 때를 체크하고, 짝 또는 currentNum을 반환한다

--- sprint-5/domain/person.js
+++ sprint-5/domain/person.js
@@ -10,7 +10,26 @@ class Person extends EventEmitter {
   checkMyTurn() {
     this.on(this.name, this.clapTime);
   }
+
+  is369(currentNum) {
+    const regExp369 = /[369]/;
+    return regExp369.test(currentNum);
+  }
+
+  isClap() {
+    return _.random(0, 1);
+  }
+
+  clapTime(currentNum) {
+    if (this.is369(currentNum)) {
+      if (this.isClap()) {
+        this.emit("receiveTo" + this.name, "짝");
+        return;
+      }
+      this.emit("receiveTo" + this.name, currentNum);
+      return;
+    }
+  }
 }
 
 module.exports = Person;
-다

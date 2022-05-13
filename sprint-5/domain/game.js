e6aabde53e1329bc1ada65b73b3a2d264f79aa27
gxxrxn <rbfks1221@naver.com>
2022-05-13 19:53:25 +0900

    feat: 369 Game 클래스 작성
    - EventEmitter 상속하는 Game 클래스 구현
    - Game 참여 플레이어 추가 기능 구현
    - Game 참여 플레이어 제거 기능 구현
    - 게임 시작 기능 구현
    - 박수쳐야하는지 판단하는 함수 작성

--- sprint-5/domain/game.js
+++ sprint-5/domain/game.js
@@ -1,5 +1,67 @@
-class Game {
-  constructor() {}
+const EventEmitter = require("events");
+
+class Game extends EventEmitter {
+  constructor() {
+    super();
+    this.players = [];
+  }
+
+  addPlayer(player) {
+    this.players.push(player);
+    return this;
+  }
+
+  removePlayer(player) {
+    this.players.filter((obj) => obj.name !== player.name);
+    return this;
+  }
+
+  startGameBy(player) {
+    if (this.players.length === 0) {
+      return;
+    }
+
+    let playerIdx = this.players.findIndex((obj) => obj.name === player.name);
+    let currentNum = 1;
+    let status = true;
+
+    while (status) {
+      playerIdx %= this.players.length;
+      const player = this.players[playerIdx];
+      const result = player.play(currentNum);
+
+      // 박수 쳐야하는 경우 (3, 6, 9가 포함된 경우)
+      if (
+        (this.isClap(currentNum) && result === "짝") ||
+        (!this.isClap(currentNum) && result === currentNum)
+      ) {
+        this.emit("pass", player.name, result);
+        playerIdx++;
+        currentNum++;
+      } else {
+        this.emit("pass", player.name, result);
+        this.emit(
+          "fail",
+          this.players
+            .filter((obj) => obj.name !== player.name)
+            .map((obj) => obj.name)
+            .join(",")
+        );
+        status = false;
+      }
+    }
+
+    return this;
+  }
+
+  isClap(num) {
+    return (
+      num
+        .toString()
+        .split("")
+        .findIndex((num) => num != 0 && num % 3 === 0) !== -1
+    );
+  }
 }
 
 module.exports = Game;

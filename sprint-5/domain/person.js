9b85920923942ce38a496eeb25d9ec1eba01658d
gxxrxn <rbfks1221@naver.com>
2022-05-13 19:57:47 +0900

    feat: 게임을 하는 Person 클래스 구현
    - 이름을 파라미터로 전달받아 생성
    - 친구와 게임을 시작할 수 있는 startWith 함수 구현
    - 숫자가 들어오면 박수를 칠지 말할지를 랜덤하게 결정하는 play 함수 구현

--- sprint-5/domain/person.js
+++ sprint-5/domain/person.js
@@ -1,5 +1,30 @@
+const Game = require("./game.js");
+
 class Person {
-  constructor() {}
+  constructor(name) {
+    this.name = name;
+  }
+
+  startWith(friend) {
+    new Game()
+      .addPlayer(this)
+      .addPlayer(friend)
+      .on("pass", (name, result) => console.log(`${name}: ${result}`))
+      .on("fail", (winner) => console.log(`${winner}가 승리했습니다.`))
+      .startGameBy(this);
+  }
+
+  play(num) {
+    if (
+      num
+        .toString()
+        .split("")
+        .findIndex((num) => num != 0 && num % 3 === 0) !== -1
+    ) {
+      return Math.floor(Math.random() * 2) ? "짝" : num;
+    }
+    return num;
+  }
 }
 
 module.exports = Person;

b3eef5aae5f9df127a438fdb77b52975e2d9b1e0
spiderq2697 <spiderq10@gmail.com>
2022-05-16 19:11:39 +0900

    feat: 객체 간 이벤트 통신을 위해 EventEmitter 인스턴스를 생성자에 전달한다

--- sprint-5/main.js
+++ sprint-5/main.js
@@ -1,15 +1,20 @@
 const Game = require("./domain/game");
 const Person = require("./domain/person");
+const EventEmitter = require("events");
 
 function main() {
-  const personA = new Person("A");
-  const personB = new Person("B");
-  const game = new Game();
+  const eventEmitter = new EventEmitter();
+
+  const personA = new Person("A", eventEmitter);
+  const personB = new Person("B", eventEmitter);
+  const personC = new Person("C", eventEmitter);
 
   personA.checkMyTurn();
   personB.checkMyTurn();
 
-  game.start(personA);
+  const game = new Game(eventEmitter, participants);
+
+  game.start();
 }
 
 main();
